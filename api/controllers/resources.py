446b82d12fd9cd1a9e48bc4bdd5202ea5166f849
CalebFilip <filip0732caleb@gmail.com>
2024-11-05 21:16:53 -0500

    feat: implemented sandwiches.py functionality

--- api/controllers/resources.py
+++ api/controllers/resources.py
@@ -4,7 +4,6 @@ from ..models import models, schemas
 
 def create(db: Session, resource):
     db_resource = models.Resource(
-        id = resource.id,
         item = resource.item,
         amount = resource.amount
     )
